54faa3e6bbc3ba4f628412b705c3e01103636d17
Vladimir <vladimirrogalev1@gmail.com>
2024-06-29 13:30:23 +0300

    changed class component to functional component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,64 +1,123 @@
-import React, {RefObject} from 'react';
+// import React, {RefObject} from 'react';
 
+// version with Class
+
+// interface Props {
+//     index: number;
+//     updateTask: (index: number, content: string) => void;
+//     deleteTask: (index: number) => void;
+//     children: string;
+// }
+//
+// interface State {
+//     isEdit: boolean;
+// }
+//
+// class Task extends React.Component <Props, State> {
+//     textId: RefObject<HTMLTextAreaElement>;
+//
+//     constructor(props: Props) {
+//         super(props);
+//         this.state = {
+//             isEdit: false
+//         };
+//         this.textId = React.createRef();
+//     }
+//
+//     handleClickEdit = () => {
+//         this.setState({
+//             isEdit: true
+//         });
+//     };
+//
+//     handleClickRemove = () => {
+//         this.props.deleteTask(this.props.index);
+//     };
+//
+//     handleClickSave = () => {
+//         const taskName = this.textId.current!.value;
+//         this.props.updateTask(this.props.index, taskName);
+//         this.setState({isEdit: false});
+//     };
+//
+//     renderView = () => {
+//         return (
+//             <div className={'box'}>
+//                 <div>{this.props.children}</div>
+//                 <button onClick={this.handleClickEdit} className={'btn light'}>Edit</button>
+//                 <button onClick={this.handleClickRemove} className={'btn red'}>Remove</button>
+//             </div>
+//         );
+//     };
+//
+//     renderEdit = () => {
+//         return (
+//             <div className={'box'}>
+//                 <textarea ref={this.textId}>New Task</textarea>
+//                 <button onClick={this.handleClickSave} className={'btn success'}>Save</button>
+//             </div>
+//         );
+//     };
+//
+//     render() {
+//         return this.state.isEdit ? this.renderEdit() : this.renderView();
+//     }
+// }
+// export default Task;
+
+// version with Function
+
+import {useRef, useState} from 'react';
 
 interface Props {
     index: number;
-    updateTask: (index: number, content: string) =>void;
-     deleteTask: (index: number) =>void;
-     children: string
-}
-interface State {
-    isEdit: boolean;
+    updateTask: (index: number, content: string) => void;
+    deleteTask: (index: number) => void;
+    children: string;
 }
 
-class Task extends React.Component <Props,State> {
-    textId: RefObject<HTMLTextAreaElement>
-    constructor(props:Props) {
-        super(props);
-        this.state = {
-            isEdit: false
-        };
-        this.textId = React.createRef();
-    }
 
-    handleClickEdit = () => {
-        this.setState({
-            isEdit: true
-        });
+const Task = (props: Props) => {
+
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+    const textId = useRef<HTMLTextAreaElement | null>(null);
+
+    const handleClickEdit = () => {
+        setIsEdit(true);
     };
 
-    handleClickRemove = () => {
-        this.props.deleteTask(this.props.index);
+    const handleClickRemove = () => {
+        props.deleteTask(props.index);
     };
 
-    handleClickSave = () => {
-        const taskName = this.textId.current!.value;
-        this.props.updateTask(this.props.index, taskName);
-        this.setState({isEdit: false});
+    const handleClickSave = () => {
+        const taskName = textId.current!.value;
+        props.updateTask(props.index, taskName);
+        setIsEdit(false);
     };
 
-    renderView = () => {
+    const renderView = () => {
         return (
             <div className={'box'}>
-                <div>{this.props.children}</div>
-                <button onClick={this.handleClickEdit} className={'btn light'}>Edit</button>
-                <button onClick={this.handleClickRemove} className={'btn red'}>Remove</button>
+                <div>{props.children}</div>
+                <button onClick={handleClickEdit} className={'btn light'}>Edit</button>
+                <button onClick={handleClickRemove} className={'btn red'}>Remove</button>
             </div>
         );
     };
 
-    renderEdit = () => {
+    const renderEdit = () => {
         return (
             <div className={'box'}>
-                <textarea ref={this.textId}>New Task</textarea>
-                <button onClick={this.handleClickSave} className={'btn success'}>Save</button>
+                <textarea ref={textId}>New Task</textarea>
+                <button onClick={handleClickSave} className={'btn success'}>Save</button>
             </div>
         );
     };
 
-    render() {
-        return this.state.isEdit ? this.renderEdit() : this.renderView();
-    }
-}
+
+    return isEdit ? renderEdit() : renderView();
+
+};
 
 export default Task;
